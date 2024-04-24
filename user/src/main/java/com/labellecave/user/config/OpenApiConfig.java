773b19a53aad886cb744c313f43fb1e1a51de03d
Robin JOSEPH <49043100+robjo82@users.noreply.github.com>
2024-04-24 11:23:13 +0200

    feat: Add new applications for Product, Payment and Order (#4)
    * feat: Add new applications for Product, Payment and Order
    
    Added new Spring Boot applications for Product, Payment, and Order modules. Created Docker files, application properties, pom files, and gitignore files for each module. Updated Docker compose file and added User service.
    
    Signed-off-by: Robin JOSEPH <rjrdutg@gmail.com>
    
    * feat: Update docker-compose to use remote image and enhance OpenAPI configuration
    
    Modified the docker-compose.yml to use a remote image instead of a local build. Adjusted OpenAPI configuration to dynamically set the server URL based on the request's server name. Additionally, a new CORS configuration class was added for enabling Cross-Origin requests.
    
    Signed-off-by: Robin JOSEPH <rjrdutg@gmail.com>
    
    * feat: Update docker-compose to use remote image and enhance OpenAPI configuration
    
    Modified the docker-compose.yml to use a remote image instead of a local build. Adjusted OpenAPI configuration to dynamically set the server URL based on the request's server name. Additionally, a new CORS configuration class was added for enabling Cross-Origin requests.
    
    Signed-off-by: Robin JOSEPH <rjrdutg@gmail.com>
    
    * feat: Update docker-compose to use remote image and enhance OpenAPI configuration
    
    Modified the docker-compose.yml to use a remote image instead of a local build. Adjusted OpenAPI configuration to dynamically set the server URL based on the request's server name. Additionally, a new CORS configuration class was added for enabling Cross-Origin requests.
    
    Signed-off-by: Robin JOSEPH <rjrdutg@gmail.com>
    
    ---------
    
    Signed-off-by: Robin JOSEPH <rjrdutg@gmail.com>

--- user/src/main/java/com/labellecave/user/config/OpenApiConfig.java
+++ user/src/main/java/com/labellecave/user/config/OpenApiConfig.java
@@ -4,14 +4,24 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${server.port:8082}")
+    private int serverPort;
+
     @Bean
     public OpenAPI customOpenAPI() {
+        String baseUrl = (serverPort == 80 || serverPort == 443 || serverPort == 8484) ? "https://labellecave-user.robin-joseph.fr" : "http://localhost:" + serverPort;
+
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme()
@@ -23,7 +33,10 @@ public class OpenApiConfig {
                         .title("User API")
                         .version("1.0")
                         .description("User API v1.0")
-                );
+                )
+                .servers(new ArrayList<>(List.of(
+                        new Server().url(baseUrl)
+                )));
     }
 
 }
